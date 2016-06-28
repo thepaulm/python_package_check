c6b90dec185935e20380f6ca60577ce9c32b687e
Paul Mikesell <pmikesell@gmail.com>
2016-06-27 20:22:47 -0700

    Added download_url to setup file

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     name="python_package_check",
     version=version,
     url="https://github.com/thepaulm/python_package_check",
+    download_url="https://github.com/thepaulm/python_package_check",
     author="Paul Mikesell",
     author_email="pmikesell@gmail.com",
     zip_safe=False,
